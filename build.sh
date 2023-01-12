05feef4ff9dc3da41ad006261507d5e918d8038f
VadimKukuzia <vadimol081@gmail.com>
2023-01-13 00:00:26 +0200

    Changed For Render Deploy1

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
 
-poetry --version
-poetry self update
-poetry install
-
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+#python manage.py migrate
